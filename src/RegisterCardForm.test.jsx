c68d797a7cbcbdcd6d6a4d355d0f9c635eef51aa
Bimali Dayananda <bimalidayananda@gmail.com>
2023-03-23 00:02:41 +1300

    Add test cases for Register form

---
+++ src/RegisterCardForm.test.jsx
@@ -0,0 +1,5 @@
+describe("RegisterCardForm component", () => {
+    it.todo("renders the RegisterCardForm with the header and CreaditCardDetails component")
+    it.todo("renders the welcome message with the user's first name")
+    it.todo("renders the credit card details form")
+})
