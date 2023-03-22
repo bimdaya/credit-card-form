8aae2b0e97381d3d002ce2d70e73840de2571925
Bimali Dayananda <bimalidayananda@gmail.com>
2023-03-22 23:56:17 +1300

    Add dropdown menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import DropDownMenu from './components/DropDownMenu'
 import RegisterCardForm from './RegisterCardForm'
 
 function App() {
   return (
     <div className="App">
-
-      <DropDownMenu />
-      <RegisterCardForm firstName={'Bim'} />
+      <Router>
+        <div>
+          <DropDownMenu />
+          <Routes>
+            <Route path="/" element={<RegisterCardForm firstName={'Bim'} />}>
+            </Route>
+          </Routes>
+        </div>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
