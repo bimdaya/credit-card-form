9ff240b1bcb9f2b8b850a7e5bc74d1abb3c66da8
Bimali Dayananda <bimalidayananda@gmail.com>
2023-03-22 23:47:24 +1300

    Calling Heading in Home

--- src/RegisterCardForm.tsx
+++ src/RegisterCardForm.tsx
@@ -1,6 +1,11 @@
 import { Grid } from "@mui/material"
+import Heading from "./components/Heading"
 
-const RegisterCardForm = () => {
+type UserProps = {
+    firstName: string
+}
+
+const RegisterCardForm = (props: UserProps) => {
 
     return (
         <>
@@ -8,6 +13,7 @@ const RegisterCardForm = () => {
                 spacing={2}
                 justifyContent="center"
                 alignItems="center">
+                <Heading>{`Welcome ${props.firstName}`}</Heading>
             </Grid>
             <Grid container
                 spacing={2}
